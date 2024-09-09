0afed1e47d974ab11951e2e18e947ab932d98380
Raso ABDOUL <raso.abdoul@gmail.com>
2024-09-09 16:02:01 +0000

    Application RESTful avec Flask

---
+++ PROJET_HOTEL_RESERVATION/BD/reservation_hotel.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 09 sep. 2024 à 17:59
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `reservation_hotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `chambres`
+--
+
+CREATE TABLE `chambres` (
+  `chambre_id` int(11) NOT NULL,
+  `chambre_number` varchar(50) NOT NULL,
+  `chambre_type` varchar(50) NOT NULL,
+  `prix_par_nuit` float NOT NULL,
+  `disponible` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `chambres`
+--
+
+INSERT INTO `chambres` (`chambre_id`, `chambre_number`, `chambre_type`, `prix_par_nuit`, `disponible`) VALUES
+(1, 'A1', 'Luxe', 20000, NULL),
+(2, 'A2', 'Single', 1000000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telephone` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `clients`
+--
+
+INSERT INTO `clients` (`id`, `nom`, `prenom`, `email`, `telephone`) VALUES
+(1, 'Dupont', 'Jean', 'jean.dupont@example.com', '0123456789'),
+(2, 'DILBOUDO', 'RAMANE', 'raso.abdoul@gmail.com', '70163867');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `chambre_id` int(11) NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `date_entree` date NOT NULL,
+  `date_sortie` date NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `chambre_id`, `client_id`, `date_entree`, `date_sortie`, `status`) VALUES
+(2, 1, 1, '2024-08-15', '2024-08-25', 'CONFIRME'),
+(3, 1, 1, '2024-08-30', '2024-09-05', 'CONFIRME'),
+(4, 2, 2, '2024-09-10', '2024-09-20', 'CONFIRME'),
+(5, 1, 2, '2024-09-25', '2024-09-30', 'ANNULEE'),
+(6, 1, 1, '2024-09-01', '2024-09-05', 'Confirmé'),
+(7, 2, 1, '0000-00-00', '2024-09-05', 'Confirmé');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `chambres`
+--
+ALTER TABLE `chambres`
+  ADD PRIMARY KEY (`chambre_id`);
+
+--
+-- Index pour la table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `chambre_id` (`chambre_id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `chambres`
+--
+ALTER TABLE `chambres`
+  MODIFY `chambre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`chambre_id`) REFERENCES `chambres` (`chambre_id`),
+  ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
